a83ac14c2a46c389ff713decb218e1e417967f7c
sanket111219 <dassanket11@gmail.com>
2022-07-05 14:35:08 +0530

    Added Medical Representative Scheduler

---
+++ MedicalRepresentativeScheduler/Controllers/RepScheduleController.cs
@@ -0,0 +1,31 @@
+﻿using MedicalRepresentativeScheduler.DataProvider;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MedicalRepresentativeScheduler.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class RepScheduleController : ControllerBase
+    {
+        public IMedicalRepresentativeSchedule medicalRepresentativeSchedule;
+
+        public RepScheduleController()
+        {
+            medicalRepresentativeSchedule = new MedicalRepresentativeSchedule();
+        }
+        // GET: api/<RepScheduleController>
+        [HttpGet("RepSchedule")]
+        public IActionResult Get(DateTime startDate)
+        {
+
+            return Ok(medicalRepresentativeSchedule.GetScedule(startDate));
+            
+        }
+
+       
+    }
+}
