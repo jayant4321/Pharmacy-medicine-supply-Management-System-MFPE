c676e9f574dc4c3b20edfd8426c1480ed10d177d
Jayant <jayantawasthi5@gmail.com>
2022-07-16 18:39:21 +0530

    Final

--- MedicalRepresentativeScheduler-tests/UnitTest1.cs
+++ MedicalRepresentativeScheduler-tests/UnitTest1.cs
@@ -1,30 +1,28 @@
+using NUnit.Framework;
 using MedicalRepresentativeScheduler;
 using MedicalRepresentativeScheduler.DataProvider;
 using MedicalRepresentativeScheduler.Models;
 using Moq;
-using NUnit.Framework;
-using System;
 using System.Collections.Generic;
+using System;
 
 namespace MedicalRepresentativeScheduler_tests
 {
-     
-
     public class Tests
     {
         List<Medicine> medicines = new List<Medicine>();
         List<RepSchedule> representatives = new List<RepSchedule>();
-       
+        string token = "alphabeta";
 
         [TestCase("30 July 2020")]
         public void GetPharmacyNames_Returns_Object(DateTime schedule)
         {
             Mock<IMedicalRepresentativeSchedule> mock = new Mock<IMedicalRepresentativeSchedule>();
 
-            mock.Setup(p => p.GetSchedule(schedule)).Returns(representatives);
+            mock.Setup(p => p.GetSchedule(schedule,token)).Returns(representatives);
 
             MedicalRepresentativeSchedule det = new MedicalRepresentativeSchedule();
-            var sch = det.GetSchedule(schedule);
+            var sch = det.GetSchedule(schedule,token);
 
             Assert.That(sch, Is.Not.Null);
         }
@@ -33,17 +31,12 @@ namespace MedicalRepresentativeScheduler_tests
         public void GetMedicineStock()
         {
             Mock<IMedicineStockProvider> mock = new Mock<IMedicineStockProvider>();
-            mock.Setup(p => p.GetMedicineList()).Returns(medicines);
+            mock.Setup(p => p.GetMedicineList(token)).Returns(medicines);
             MedicineStockProvider pro = new MedicineStockProvider();
 
-            var penCred = pro.GetMedicineList();
+            var penCred = pro.GetMedicineList(token);
 
             Assert.IsNotNull(penCred);
         }
-
-        
-
     }
 }
-
-
